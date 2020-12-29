645fe06ad14745b410bde365ccbd04f854bf7ed6
Thürzu <66086806+Thuerzu@users.noreply.github.com>
2020-12-29 18:05:14 +0100

    Update Terrain base generator.py

--- Terrain base generator.py
+++ Terrain base generator.py
@@ -1,1 +1,7 @@
+import random
+import time
 
+def get_chunk_biome_data(chunk_x, chunk_y): #returns list: [biomedata_colums, biomedata_left_chunk, biomedata_right_chunk]
+	pass
+def generate_chunk(chunk_x, chunk_y): #returns a 2d array of objects
+	pass
